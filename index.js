b099ec6781a78911b12cedba65414babdaf62ee1
Taylor Piccarreto <tpic12@gmail.com>
2019-12-29 17:38:47 -0500

    fix semantic html and acceesibility

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 //burger menu appear then disappear
 let menu = document.getElementById('menu'),
-    btn = document.querySelector('button');
+    btn = document.querySelector('.burger-button');
 
-btn.addEventListener('click', function () {
-  
+btn.addEventListener('keypress', function (event) {
+  if (event.keyCode === 13) {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden');
     setTimeout(function () {
@@ -21,8 +21,29 @@ btn.addEventListener('click', function () {
       passive: false
     });
   }
-  
+}
 }, false);
+
+
+btn.addEventListener('click', function () {
+  
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+      setTimeout(function () {
+        menu.classList.remove('visuallyhidden');
+      }, 20);
+    } else {
+      menu.classList.add('visuallyhidden');    
+      menu.addEventListener('transitionend', function(e) {
+        menu.classList.add('hidden');
+      }, {
+        capture: false,
+        once: true,
+        passive: false
+      });
+    }
+    
+  }, false);
 
 //menu links to sections on page
 const aboutMe = document.getElementById('about-btn');
@@ -74,15 +95,24 @@ let getTopSkills = function(){
 
     return x;
 }
-
 aboutMe.addEventListener('click', function(){
     const xy = document.body.getBoundingClientRect();
-    console.log(xy);
     window.scrollTo({
         top: getTopAbout(),
         left: xy.x,
         behavior: 'smooth'
     })
+
+});
+aboutMe.addEventListener('keypress', function(event){
+    if (event.keyCode ===13) {
+      const xy = document.body.getBoundingClientRect();
+      window.scrollTo({
+        top: getTopAbout(),
+        left: xy.x,
+        behavior: 'smooth'
+      })
+    }
 });
 
 projects.addEventListener('click', function(){
@@ -94,6 +124,17 @@ projects.addEventListener('click', function(){
     })
 });
 
+projects.addEventListener('keypress', function(event){
+  if (event.keyCode === 13){
+    const xy = projectsSection.getBoundingClientRect();
+    window.scrollTo({
+        top: getTopProject(),
+        left: xy.x,
+        behavior: 'smooth'
+    })
+  }
+});
+
 skills.addEventListener('click', function(){
     const xy = skillsSection.getBoundingClientRect();
     window.scrollTo({
@@ -101,4 +142,15 @@ skills.addEventListener('click', function(){
         left: xy.x,
         behavior: 'smooth'
     })
+});
+
+skills.addEventListener('keypress', function(event){
+  if(event.keyCode === 13) {
+    const xy = skillsSection.getBoundingClientRect();
+    window.scrollTo({
+        top: getTopSkills(),
+        left: xy.x,
+        behavior: 'smooth'
+    })
+  }
 });
